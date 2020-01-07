500333860cb92d482367fc4aa263b01ae7c3131f
David R <mineawesomeman@gmail.com>
2020-01-07 08:36:22 -0500

    uwu

--- functions.py
+++ functions.py
@@ -1,17 +1,46 @@
 import pymysql as mc
 import sys
+from datetime import datetime as dt
 
 connection = None
 cursor = None
 
+##Set up all of the variables
 def init():
 	global connection
 	global cursor
 	connection = mc.connect (host = "localhost",user = "pytester",passwd = "monty",db = "SchoolConnect")
 	cursor = connection.cursor()
 
-def getClasses(ID):
+
+##Class Information
+def getClassIDs(ID):
 	global connection
 	global cursor
-	cursor.execute("SELECT * FROM StudentClass WHERE ID=" + str(ID) + ";")
-	cursor.fetchall()
+	cursor.execute("SELECT ClassID FROM StudentClass WHERE StudentID=" + str(ID) + ";")
+	classIDs = []
+	fetch = cursor.fetchall()
+	for row in fetch:
+		classIDs.append(row[0])
+	return classIDs
+
+def getClassInfo(ID):
+	global connection
+	global cursor
+	info = []
+	classIDs = getClassIDs(ID)
+	for ids in classIDs:
+		cursor.execute("SELECT * FROM Classes WHERE ID=" + str(ids) + ";")
+		info.append(cursor.fetchall()[0])
+	return info
+
+#Announcement Functions
+def getAnnouncements(classes, sdate='1970-01-01 00:00:00', edate='', count=50):
+	if edate == '':	
+		edate = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+	classString = "(Class IN (-1"
+	for ID in classes:
+		classString = classString + "," + str(ID)
+	classString = classString + "))"
+	cursor.execute("SELECT * FROM Announcements WHERE " + classString + " AND (Time BETWEEN \"" + sdate + "\" AND \"" + edate + "\") ORDER BY Time DESC LIMIT " + str(count) + ";")
+	return cursor.fetchall()
